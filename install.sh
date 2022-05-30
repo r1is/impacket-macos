cd305ae55e5735d6805c4f6c2af542f5cca0d972
r1is <iryzezr@gmail.com>
2022-05-30 23:55:36 +0800

    fix

--- install.sh
+++ install.sh
@@ -4,14 +4,14 @@ file_url="https://github.com/SecureAuthCorp/impacket.git"
 file_name="impacket"
 
 # 下载impacket文件
-git clone file_url
+git clone ${file_url}
 
 if [ $? -ne 0 ];then
     exit -1
 fi
 
 sudo mv $file_name /opt
-cd /opt/${file_name}
+cd /opt/${file_name} && sudo chown -R $(whoami):staff /opt/${file_name}
 
 script_file="script"
 cat >${script_file}<<EOF
@@ -22,15 +22,11 @@ example_name=\${name_script##impacket-}
 
 exec python3 /opt/impacket/examples/\$example_name.py "\$@"
 EOF
-
-if [ -x "$script_file" ];then
-    chmod +x $script_file
+if [ -f "$script_file" ];then
+    chmod +x $script_file && cat $script_file
 else
     exit -1
 fi
-
-# 安装 impacket 工具包
-python3 setup.py install
 
 # 创建快捷方式 impacket-xxxx
 src_py="/opt/impacket/${script_file}"
@@ -40,3 +36,4 @@ for str in `ls examples`;do
     ln -s ${src_py} ${link}${examples_name}
     chmod +x ${link}${examples_name}
 done
+sudo chown -R $(whoami):staff /usr/local/bin/impacket-*
